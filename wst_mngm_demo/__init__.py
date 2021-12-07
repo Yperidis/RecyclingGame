6dee80b9f63e5cc35fdaac8007c62d6826b16d88
Bassett <bassett@chm-lin-004945.mpib-berlin.mpg.de>
2021-12-07 19:01:32 +0100

    Fixed the UC validation fields and modified the recursive capac formula

--- wst_mngm_demo/__init__.py
+++ wst_mngm_demo/__init__.py
@@ -1,6 +1,4 @@
 from otree.api import *
-from wst_mngm_demo.payoffs import *  # not working. Figure out how to distribute the app across py files
-from wst_mngm_demo.utils import *  # likewise
 
 
 doc = """
@@ -30,7 +28,7 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):  
     # Action set: actionS for "store", actionPP for "push on platform" and actionD for "Dispose through standard means"
-    actionS = models.IntegerField(min=0, max=Constants.g, label="How many items do you want to store?")
+    actionS = models.IntegerField(min=0, max=Constants.g, initial=0, label="How many items do you want to store?")
     actionPP = models.IntegerField(min=0, max=Constants.g+Constants.Cmax, label="How many items do you want to push to the platform?")
     actionD = models.IntegerField(min=0, max=Constants.g+Constants.Cmax, label="How many items do you want to dispose through standard means?")
     actionFwd = models.IntegerField(min=0, label="How many items do you want to forward to another CH?")
@@ -41,29 +39,7 @@ class Player(BasePlayer):
 # PAGES
 class Days(Page):
     form_model = 'player'
-    # form_fields = ['actionS', 'actionPP', 'actionD', 'actionFwd', 'actionRESell', 'WstType']  # the action set
-
-    @staticmethod
-    def UC_error_message(player, actions):
-        # print('The actions are', actions)
-        if player.role == Constants.UC_role:
-            LHS, RHS = actions['actionS'] + actions['actionPP'] + actions['actionD'], Constants.g + Constants.Cmax - player.participant.capac
-            # print(player.participant.capac)
-            if LHS != RHS:
-                return 'The sum of the stored items, pushed to platform and otherwise disposed must equal the generated waste items for all rounds.'
-
-    @staticmethod
-    def CH_error_message(player, actions):
-        # print('The actions are', actions)
-        if player.role == Constants.CH_role:
-            if player.round_number == 1:
-                return 'Initializing. Nothing to forward or sell yet.'
-            else:
-                LHS = actions['actionFwd'] + actions['actionRESell']
-                RHS = actions['actionPP'] + Constants.Cmax - player.participant.capac  # TODO pick a UC and include what they pushed in the round at hand (the criteria for the picked one are: 1. that the CH maximizes their profit, 2. that the CH is closest to the UC and 3. that the UC is willing to pair)
-                if LHS != RHS:  # what a CH forwards or sells cannot be more than what they can carry and what they already have in store
-                    return 'You cannot forward to another CH or sell to an RE more than you have.'
-
+    # form_fields = ['actionS', 'actionPP', 'actionD', 'WstType']  # the action set
 
     @staticmethod
     def get_form_fields(player):
@@ -71,6 +47,21 @@ class Days(Page):
             return ['actionS', 'actionPP', 'actionD', 'WstType']
         elif player.role == Constants.CH_role and player.round_number > 1:
             return ['actionS', 'actionFwd', 'actionRESell', 'WstType']
+
+    @staticmethod
+    def error_message(player, actions):
+        if player.role == Constants.UC_role:
+            LHS, RHS = actions['actionS'] + actions['actionPP'] + actions['actionD'], Constants.g + Constants.Cmax - player.participant.capac
+            if LHS != RHS:
+                return 'The sum of the stored items, pushed to platform and otherwise disposed must equal the generated waste items for all rounds.'
+        # elif player.role == Constants.CH_role:
+        #     if player.round_number == 1:
+        #         return 'Initializing. Nothing to forward or sell yet.'
+        #     else:
+        #         LHS = actions['actionS'] + actions['actionFwd'] + actions['actionRESell']
+        #         RHS = actions['actionPP'] + Constants.Cmax #- player.participant.capac  # TODO pick a UC and include what they pushed in the round at hand (the criteria for the picked one are: 1. that the CH maximizes their profit, 2. that the CH is closest to the UC and 3. that the UC is willing to pair)
+        #         if LHS != RHS:  # what a CH forwards or sells cannot be more than what they can carry and what they already have in store
+        #             return 'You cannot forward to another CH or sell to an RE more than you have.'
 
 
 class ResultsWaitPage(WaitPage):
@@ -88,62 +79,40 @@ def creating_session(subsession):
     subsession.group_randomly(fixed_id_in_group=True)  # for grouping players randomly upon initialisation but keeping roles constant throughout the rounds    
 
     for player in players:
-        if subsession.round_number == 1 and player.role == Constants.UC_role or player.role == Constants.CH_role:
-            player.participant.capac = Constants.Cmax  # this is to maintain the capacity over all rounds
-            # print(player.participant.capac, player.role)
-        elif subsession.round_number > 1 and player.role == Constants.UC_role or player.role == Constants.CH_role:
+        if subsession.round_number == 1 and (player.role == Constants.UC_role or player.role == Constants.CH_role):
+            player.participant.capac = Constants.Cmax  # initialise capacity
+            print(player.participant.capac, player.role, player.round_number)
+        elif subsession.round_number > 1 and (player.role == Constants.UC_role or player.role == Constants.CH_role):
             prev_player = player.in_round(subsession.round_number - 1)
             # print(prev_player.round_number, prev_player.role)
-            player.participant.capac = Constants.Cmax - prev_player.participant.capac  # this is to maintain the capacity over all rounds
-
-
-def find_match():
-    pass
+            player.participant.capac = Constants.Cmax - prev_player.actionS  # calculate capacity for next round
+            print(player.participant.capac, player.role)
 
 
 def set_payoffs(subsession):
     players = subsession.get_players()
-
     for player in players:
         if player.role == Constants.UC_role:
-            UC_Payoff(subsession, player)
-        elif player.role == Constants.CH_role:
-            CH_Payoff(subsession, player)
-
-
-def UC_Payoff(subsession, player):
-    wsttype = player.WstType  # the specified item to be exchanged TODO: check whether it's in the constant list
-    if subsession.round_number == 1:  # first round
-        if Constants.Cmax >= Constants.g:  # check whether the capacity is greater than the waste generation 
-            if player.actionD > 0:
-                player.payoff = player.actionPP * Constants.ItemDep[wsttype] - Constants.OpTariff  # payoff formula for storing, pushing to platform and flat rate for using the stadard disposal means
-                player.participant.capac = Constants.Cmax - player.actionS  # track the capacity in case of storage action
+            wsttype = player.WstType  # the specified item to be exchanged TODO: check whether it's in the constant list
+            if subsession.round_number == 1:  # first round
+                if Constants.Cmax >= Constants.g:  # check whether the capacity is greater than the waste generation 
+                    if player.actionD > 0:
+                        player.payoff = player.actionPP * Constants.ItemDep[wsttype] - Constants.OpTariff  # payoff formula for storing, pushing to platform and flat rate for using the stadard disposal means
+                        player.participant.capac = Constants.Cmax - player.actionS  # track the capacity in case of storage action
+                    else:
+                        player.payoff = player.actionPP * Constants.ItemDep[wsttype]  # payoff formula for storing and pushing to platform
+                        player.participant.capac = player.actionS  # track the capacity in case of storage action
+                else:
+                    raise ValueError('The player generates more than they can handle.')  
             else:
-                player.payoff = player.actionPP * Constants.ItemDep[wsttype]  # payoff formula for storing and pushing to platform
-                player.participant.capac = player.actionS  # track the capacity in case of storage action
-        else:
-            raise ValueError('The player generates more than they can handle.')  
-    else:
-        prev_player = player.in_round(subsession.round_number - 1)
-        if prev_player.participant.capac >= player.actionS:  # in case of sufficient storage
-            if player.actionD > 0:
-                player.payoff = player.actionPP * Constants.ItemDep[wsttype] - Constants.OpTariff  # payoff formula for storing, pushing to platform and flat rate for using the stadard disposal means
-                player.participant.capac = prev_player.participant.capac - player.actionS + player.actionPP - Constants.g + player.actionD - Constants.g  # recursive relation to track the player's item capacity
-            else:
-                player.payoff = player.actionPP * Constants.ItemDep[wsttype]  # payoff formula for storing and pushing to platform
-                player.participant.capac = prev_player.participant.capac - player.actionS + player.actionPP - Constants.g  # recursive relation to track the player's item capacity (no stanard disposal)
-
-def CH_Payoff(subsession, player):
-    if subsession.round_number == 1:  # first round
-        player.payoff = 0
-    else:
-        wsttype = player.WstType  # the specified item to be exchanged TODO: check whether it's in the constant list
-        prev_player = player.in_round(subsession.round_number - 1)
-        if prev_player.participant.capac >= player.actionS:  # in case of sufficient storage
-            if player.actionFwd > 0 or player.actionRESell > 0:
-                player.payoff = player.actionFwd * Constants.ItemDep[wsttype] + player.actionRESell * Constants.ItemDep[wsttype]  # payoff formula for forwarding to another CH pushing
-                player.capac = prev_player.capac - player.actionS + player.actionPP - Constants.g + player.actionD - Constants.g  # recursive relation to track the player's item capacity
+                prev_player = player.in_round(subsession.round_number - 1)
+                if prev_player.participant.capac >= player.actionS:  # in case of sufficient storage
+                    if player.actionD > 0:
+                        player.payoff = player.actionPP * Constants.ItemDep[wsttype] - Constants.OpTariff  # payoff formula for storing, pushing to platform and flat rate for using the stadard disposal means
+                        player.participant.capac = prev_player.participant.capac - player.actionS + player.actionPP - Constants.g + player.actionD - Constants.g  # recursive relation to track the player's item capacity
+                    else:
+                        player.payoff = player.actionPP * Constants.ItemDep[wsttype]  # payoff formula for storing and pushing to platform
+                        player.participant.capac = prev_player.participant.capac - player.actionS + player.actionPP - Constants.g  # recursive relation to track the player's item capacity (no stanard disposal)
 
 
 page_sequence = [Days, ResultsWaitPage, Results]
-
