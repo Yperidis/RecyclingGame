92ed633f5db900ae8616cb1a6fbb9078ae06fb93
Bassett <ligistis@yahoo.gr>
2022-06-16 19:18:20 +0200

    Fixed reinitialization after trials and added text to the static HTML treatment.

--- wst_mngm_main/utils.py
+++ wst_mngm_main/utils.py
@@ -3,7 +3,6 @@ def Initialization(subsession, Constants):
     # subsession.set_group_matrix(new_structure)
     groups = subsession.get_groups()
     players = subsession.get_players()
-    # subsession.group_randomly(fixed_id_in_group=True)  # for grouping players randomly upon initialisation but keeping roles constant throughout the rounds    
     roles = ['UC', 'UC', 'CH']  # assuming two roles and six players per group, the loop below will always distribute the roles cyclically up to the number of players in the group
     num_UCCH = len(roles)
 
@@ -31,15 +30,11 @@ def Initialization(subsession, Constants):
 
 def ResetFields(group, Constants):
     players = group.get_players()
-    roles = ['UC', 'CH']  # assuming two roles, the loop below will always distribute the roles cyclically up to the number of players in the group
-    num_UCCH = len(roles)    
+    roles = ['UC', 'UC', 'CH']  # assuming two roles and six players per group, the loop below will always distribute the roles cyclically up to the number of players in the group
+    num_UCCH = len(roles)
+
     for player in players:
-        # if player.id_in_group == 1:
-        #     player.role_own = roles[2]  # RE assignment
-        if player.id_in_group % num_UCCH == 0:
-            player.role_own = roles[0]  # UC assignment
-        else:
-            player.role_own = roles[1]  # CH assignment
+        player.role_own = roles[player.id_in_group % num_UCCH]  # 4 UC and 2 CH players according to roles previously defined
         if player.round_number == 1:# and (player.role_own == 'UC' or player.role_own == 'CH'):
             if player.role_own == 'UC':
                 player.participant.capac = Constants.UCCmax  # initialise capacity as it is going to appear on Days.html before being affected (see payoffs)
@@ -49,4 +44,3 @@ def ResetFields(group, Constants):
                 player.participant.capac = Constants.CHCmax
                 player.participant.balance = Constants.InitCHBalance  # initialize balance for CH
             player.participant.store = 0  # initialize storage as it is going to appear on Days.html before being affected (see payoffs)
-            player.participant.DropoutCounter = 0  # initializing the drop-out counter for each player
